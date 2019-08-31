c2ca89d921297984c6ab9229a064a5bc4a7b9094
kusa-mochi <whoatemyapplepie@gmail.com>
2019-08-31 17:07:07 +0900

    add event listener to alert domain when user access sites.

--- src/background.js
+++ src/background.js
@@ -1,2 +1,25 @@
-chrome.browserAction.setBadgeText({ text: "OK" });
-chrome.browserAction.setBadgeBackgroundColor({ color: [44, 148, 60, 100] });
+function IsWhiteDomain(domain) {
+
+    //TODO: check if a domain is in user's white list.
+    
+    return true;
+}
+
+function WhiteDomain() {
+    chrome.browserAction.setBadgeBackgroundColor({ color: [44, 148, 60, 100] });
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete' && tab.active) {
+            var tablink = tab.url;
+            var domain = tablink.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+            if(IsWhiteDomain(domain) == true) {
+                chrome.browserAction.setBadgeText({ text: "SAFE" });
+            }
+            else {
+                chrome.browserAction.setBadgeText({ text: "" });
+            }
+            alert(domain);
+        }
+    });
+}
+
+window.onload = WhiteDomain;

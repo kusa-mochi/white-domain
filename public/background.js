0831f4ab73e5aec7ca12e483bef55bb91fdb924c
kusa-mochi <whoatemyapplepie@gmail.com>
2019-09-01 12:11:41 +0900

    make badge icon be labeled "SAFE" when you switch tab to a site that you added to your white domain list.

--- public/background.js
+++ public/background.js
@@ -1,3 +1,11 @@
+function GetDomainFromUrl(url) {
+    return url.replace('http://www.', '')
+        .replace('https://www.', '')
+        .replace('http://', '')
+        .replace('https://', '')
+        .split(/[/?#]/)[0];
+}
+
 function IsWhiteDomain(domain) {
 
     //TODO: check if a domain is in user's white list.
@@ -17,19 +25,30 @@ function IsWhiteDomain(domain) {
     );
 }
 
+function BadgeText(url) {
+    const domain = GetDomainFromUrl(url);
+    return IsWhiteDomain(domain) ? "SAFE" : "";
+}
+
+function SetBadgeTest(url) {
+    chrome.browserAction.setBadgeText({ text: BadgeText(url) });
+}
+
 function WhiteDomain() {
     chrome.browserAction.setBadgeBackgroundColor({ color: [44, 148, 60, 100] });
+
+    // タブが更新されたときに実行する処理
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         if (changeInfo.status == 'complete' && tab.active) {
-            var tablink = tab.url;
-            var domain = tablink
-                            .replace('http://www.', '')
-                            .replace('https://www.', '')
-                            .replace('http://', '')
-                            .replace('https://', '')
-                            .split(/[/?#]/)[0];
-            chrome.browserAction.setBadgeText({ text: IsWhiteDomain(domain) ? "SAFE" : "" });
+            SetBadgeTest(tab.url);
         }
+    });
+
+    // タブが切り替わったときに実行する処理
+    chrome.tabs.onActivated.addListener(function (tabId) {
+        chrome.tabs.query({ "active": true }, function (tab) {
+            SetBadgeTest(tab[0].url);
+        });
     });
 }
 
